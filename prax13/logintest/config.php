c80869ac23dc8f625b177ebaa8ed4a21ebfd1790
Andreas Porman <vanporman@gmail.com>
2017-05-14 15:35:21 +0300

    Delete config.php

--- prax13/logintest/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'test');
-   define('DB_PASSWORD', 't3st3r123');
-   define('DB_DATABASE', 'test');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
