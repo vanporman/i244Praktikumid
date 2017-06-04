05e5b93fae4ecba48f685a436d0cb32d425ae53b
Andreas Porman <aporman@itcollege.ee>
2017-06-04 15:59:35 +0300

    eksam yl valmis

--- eksam/p2riseksam/funk.php
+++ eksam/p2riseksam/funk.php
@@ -18,17 +18,17 @@ if(isset($_POST['saada'])){
     if (isset($_POST['nimi']) && $_POST['nimi'] != ""){
         $query = "INSERT INTO vanporman_parimadpakkumised (hind, nimi) VALUES('$hind', '$nimi')";
         $result = mysqli_query($connection, $query) or die("$query - " . mysqli_error($connection));
-    } else {
-        echo "NIMI PUUDU";
     }
 }
 
 $query2 = "SELECT hind, nimi FROM vanporman_parimadpakkumised WHERE hind = (SELECT Max(hind) AS parimpakkumine FROM vanporman_parimadpakkumised)";
 $result2 = mysqli_query($connection, $query2) or die("$query2 - " . mysqli_error($connection));
 while ($pakkumine = $result2 -> fetch_assoc()){
+
 	echo "<h3>Siin tuuakse välja kõige parema pakkumise teinud isik ja summa</h3>";
 	echo "<p>Kõige parema pakkumise on teinud praegu: ".$pakkumine['nimi']."</p>";
-    echo "<p>Kasutaja ".$pakkumine['nimi']." pakkumine on <b>".$pakkumine['hind']." eurot.</b></p>";
+	echo "<p>Kasutaja ".$pakkumine['nimi']." pakkumine on <b>".$pakkumine['hind']." eurot.</b></p>";
+	
 }
 mysqli_close($connection);
 ?>
